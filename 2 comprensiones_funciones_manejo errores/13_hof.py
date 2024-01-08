e2d67370080fc63a00d8099e618c59184b08f4ca
martin <martin_rps_9819@outlook.com>
2024-01-08 09:58:27 -0500

    ingreso de archivos

--- 2 comprensiones_funciones_manejo errores/13_hof.py
+++ 2 comprensiones_funciones_manejo errores/13_hof.py
@@ -3,7 +3,19 @@
 def increment(x):
     return x +1
 
+increment_v2 = lambda x: x+1
+
 def high_ord_func(x,func):
     return x + func(x)
+high_ord_func_v2 = lambda x, func: x+func(x)
 
 result = high_ord_func(2,increment)
+print(result)
+
+result = high_ord_func_v2(2, increment_v2)
+print(result)
+result = high_ord_func_v2(2, lambda x: x+2)
+print(result)
+##changing lambda function
+result = high_ord_func_v2(2, lambda x: x*5)
+print(result)
