94aacd89df9fc640882c362db973e2c64bace744
martin <martin_rps_9819@outlook.com>
2024-01-12 05:21:49 -0500

    Insercion de comandos de docker

--- 3_Curso_PIP_Entornos_Virtuales/app/main.py
+++ 3_Curso_PIP_Entornos_Virtuales/app/main.py
@@ -1,16 +1,26 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  """
   #continente = input("Type the continent: ")
   data = list(filter(lambda item : item['Continent'] in item['Continent'],data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
+  """
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent']== df['Continent']]
+
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
   
+  charts.generate_pie_chart(countries,percentages)
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
 
