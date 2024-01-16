c6f4d29146584fcf6da10c980072784983dd996e
martin <martin_rps_9819@outlook.com>
2024-01-16 11:10:03 -0500

    metodos GET POST PUT Y DELETE agregados y conexion a base datos establecido con sqlalchemy

--- 4_Curso_FastAPI/proyecto/main.py
+++ 4_Curso_FastAPI/proyecto/main.py
@@ -1,75 +1,85 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from variables import movies, User, Movie, JWTBearer
+from fastapi import FastAPI, Body, Path, Query, Depends
+from fastapi.responses import HTMLResponse,JSONResponse
+from typing import List
+from jwt_manager import create_token
+from fastapi.security.http import HTTPAuthorizationCredentials
+from Config.database import Session,engine,Base
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
+
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
-movies = [{
-        "id": 1,
-		"title": "Avatar",
-		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-		"year": "2009",
-		"rating": 7.8,
-		"category": "Acción"
-    },
-    {
-        "id": 2,
-		"title": "Avatar",
-		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-		"year": "2009",
-		"rating": 7.8,
-		"category": "Acción"
-    }      
-]
+#GENERACION DE LOGUEO DE USER
+@app.post('/login',tags=['auth'])
+def login(user:User):
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token:str= create_token(user.dict())
+    return JSONResponse(status_code=200,content=token)
 
-@app.get('/', tags=['home'])
-def message():
-    return HTMLResponse('<h1>Hello world!</1>')
+#AGREGAR UNA PELICULA A LA LISTA
+@app.post('/movies/', tags=['movies'],response_model=dict,status_code=201)
+def create_movie(movie:Movie) -> dict:
+    db = Session()
+    new_movie = MovieModel(**movie.dict())
+    db.add(new_movie)
+    db.commit()
+    return JSONResponse(status_code=201,content={"message":"Se agrego la pelicula con exito"})
 
-@app.post('/movies/', tags=['movies'])
-def create_movie(id:int=Body(),title:str=Body(),overview:str=Body(),year:str=Body(),rating:float=Body(),category:str=Body()):
-    movies.append({
-        "id":id,
-        "title":title,
-        "overview":overview,
-        "year":year,
-        "rating":rating,
-        "category":category
-    })
-    return movies
+#OBTENER LISTA TOTAL DE PELICULAS
+@app.get('/movies', tags=['movies'],response_model=List[Movie],status_code=200,dependencies=[Depends(JWTBearer())])
+def get_movies() -> List[Movie]:
+    db = Session()
+    results = db.query(MovieModel).all()
+    return JSONResponse(status_code=200,content=jsonable_encoder(results))
 
-@app.get('/movies', tags=['movies'])
-def get_movies():
-    return movies
+#OBTENER UNA PELICULA POR SELECCION DE ID
+@app.get('/movies/{id}', tags=['movies'],response_model=List[Movie])
+def get_movie(id: int = Path(ge=1,le=2000)) -> List[Movie]:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        JSONResponse(status_code=404,content={'message': 'No encontrado'})
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int):
-    for item in movies:
-        if item["id"] == id:
-            return item
-    return []
+#OBTENER LISTA DE PELICULAS QUE CUMPLAN EL FILTRO
+@app.get('/movies/', tags=['movies'],response_model=List[Movie])
+def get_movies_by_category(category: str= Query(min_length=4,max_length=500), year:int =Query(ge=1600,le=2024)) -> List[Movie]:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.category == category or MovieModel.year == year).all()
+    if not result:
+        return JSONResponse(status_code=404,content={'message':'Not found'})
+    #data = [ item for item in movies if item['year'] == str(year) and item["category"] == category]
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
-@app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category: str, year:int):
-    return [ item for item in movies if item['year'] == str(year) and item["category"] == category]
+#MODIFICAR INFO DE PELICULA POR IDENTIFICACION DE ID
+@app.put('/movies/{id}', tags=['movies'],response_model=dict,status_code=200)
+def put_movies(id:int,movie:Movie)-> dict:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404,content={'message':'Not found'})
+    result.title = movie.title
+    result.overview = movie.overview
+    result.year = movie.year
+    result.rating = movie.rating
+    result.category = movie.category
+    db.commit()
+    return JSONResponse(status_code=200,content={"message":"Se modifico la pelicula con exito"})
 
-@app.put('/movies/{id}', tags=['movies'])
-def put_movies(id:int,title:str=Body(),overview:str=Body(),year:str=Body(),rating:float=Body(),category:str=Body()):
-    for item in movies:
-        if item["id"] == id:
-            item["title"] = title,
-            item["overview"] = overview,
-            item["year"] = year,
-            item["rating"] = rating,
-            item["category"] = category
-    return movies
-@app.delete('/movies/{id}', tags=['movies'])
-def delete_movie(id:int):
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-    return movies
+#ELIMINAR PELICULA IDENTIFICADA        
+@app.delete('/movies/{id}', tags=['movies'],response_model=dict,status_code=200)
+def delete_movie(id:int)-> dict:
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404,content={'message':'Not found'})
+    db.delete(result)
+    db.commit()
+    return JSONResponse(status_code=200,content={'message':'Se elimino con exito'})
 
 
     
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# description
+# Description
 clases que se estan desarrollando para el aprendizaje del lenguaje python
-# Game project
+# Capitulo 2: Game project
 Para correr el juego debes seguir las siguientes instrucciones en la terminal:
 ´´´sh
 cd game
 python3 main.py
 ´´´
-# App project
+# Capitulo 3: App project
 
 ´´´´
 git clone
@@ -16,3 +16,16 @@ source tutorial/scripts/activate
 pip install requirements.txt
 python main.py
 ´´´
+# Capitulo 4: Dependencias de proyecto
+Esta es una lista de los comandos usados en la terminal hasta el momento:
+
+###Crear carpeta de virtualizacion
+proyecto-env\Scripts\activate
+source proyecto-env/Scripts/activate
+
+###Levantar pagina usando uvicorn
+uvicorn main:app --reload --port 5000
+
+###SQlite y sqlalchemy es necesario
+pip install sqlalchemy
+descargar sqlite view si estas usando visual studio code
